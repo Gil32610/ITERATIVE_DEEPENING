42d38985593a6bff3926b955837c9131d642de84
GIL32610 <gil.00000032610@unicap.br>
2024-03-13 17:32:41 -0300

    DFS algorithm

--- app/src/main/java/iterative/deepening/Graph.java
+++ app/src/main/java/iterative/deepening/Graph.java
@@ -1,11 +1,16 @@
 package iterative.deepening;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Graph {
     private Boolean[][] adjacencyMatrix;
     private ArrayList<GraphNode> nodeList;
     private int time;
+    private int[] start;
+    private int[] finish;
+    private Character[] previous;
+    
 
     public Graph(ArrayList<GraphNode> nodeList) {
         this.nodeList = nodeList;
@@ -44,9 +49,9 @@ public class Graph {
     }
 
     public void depthFirstSearch(int s) {
-        int[] start = new int[nodeList.size()];
-        int[] finish = new int[nodeList.size()];
-        Character[] previous = new Character[nodeList.size()];
+         start = new int[nodeList.size()];
+         finish = new int[nodeList.size()];
+         previous = new Character[nodeList.size()];
         for (int index = 0; index < nodeList.size(); index++) {
             nodeList.get(index).setColor(Color.WHITE);
             start[index] = -2;
@@ -54,6 +59,30 @@ public class Graph {
             previous[index]= null;
         }
         time = -1;
+        dfsVisit(s);
+        searchResult();
+    }
+    private void searchResult(){
+        System.out.println("DFS result:");
+        System.out.println("Time of discover for each node:");
+        System.out.println(Arrays.toString(start));
+        System.out.println("Time of finish for each node");
+        System.out.println(Arrays.toString(finish));
+        System.out.println("Parentized Structure: ");
+        System.out.println(Arrays.toString(previous));
+    }
+
+    private void dfsVisit(int u){
+        start[u] = ++time;
+        nodeList.get(u).setColor(Color.GRAY);
+        for (int v = 0; v < adjacencyMatrix.length; v++) {
+            if(adjacencyMatrix[u][v] && nodeList.get(v).getColor().equals(Color.WHITE)){
+                previous[v] = nodeList.get(u).getIdentity();
+                dfsVisit(v);
+            }
+        }
+        nodeList.get(u).setColor(Color.BLACK);
+        finish[u] = ++time;
     }
 
 }
