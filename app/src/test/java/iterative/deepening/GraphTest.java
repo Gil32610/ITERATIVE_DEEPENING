3588f950336be350a009182771c9c15f3c802616
GIL32610 <gil.00000032610@unicap.br>
2024-03-13 17:38:41 -0300

    initial test

--- app/src/test/java/iterative/deepening/GraphTest.java
+++ app/src/test/java/iterative/deepening/GraphTest.java
@@ -3,9 +3,12 @@ package iterative.deepening;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
  class GraphTest {
-    
+    private ArrayList<GraphNode> nodeList; 
     @Test void graphConstructor(){
-        
+        Character[] nodes = {'A', 'B', 'C', 'D', 'F'};
+
     }
 }
